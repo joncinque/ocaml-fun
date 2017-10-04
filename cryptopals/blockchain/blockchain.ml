541b22a289ea0313f91ee44b1dcc182fc52dfdf4
Jon Cinque <jon.cinque@gmail.com>
2017-10-04 14:40:41 -0400

    Add snazzier blockchain using mutual recursion

--- cryptopals/blockchain/blockchain.ml
+++ cryptopals/blockchain/blockchain.ml
@@ -1,14 +1,12 @@
 open Convert
 
-type 'a hash_pointer = { hash : string; data : 'a }
-type 'a blockchain = Nil | Data of 'a hash_pointer
-
 module type Hashable = sig 
   type t
   val hash : t -> string
+  val string_of_t : t -> string
 end
 
-module Make_blockchain(Data : Hashable) = struct
+module Make_blockchain_first(Data : Hashable) = struct
 
   type hash_pointer = Null | Ptr of string
   type hash_data = Empty | Data of Data.t
@@ -28,6 +26,10 @@ module Make_blockchain(Data : Hashable) = struct
     | Node({ ptr = Null  ; data = Empty  }, _) -> 
         raise (Invalid_argument "Block has neither pointer nor data, impossible")
 
+  let string_of_hash_pointer = function
+    | Null -> ""
+    | Ptr s -> s
+
   let rec verify = function
     | Nil -> true
     | Node({ ptr = Null  ; data = Data d }, _) -> true
@@ -38,7 +40,7 @@ module Make_blockchain(Data : Hashable) = struct
       if s = hash_of_node n then verify n else false
     end
 
-  let empty = Nil
+  let empty = Null
 
   let add h d = match h with
     | Nil -> begin
@@ -58,13 +60,65 @@ module Make_blockchain(Data : Hashable) = struct
 
 end
 
+module Make_blockchain(Data : Hashable) = struct
+
+  (** Core functionality *)
+  type hash_pointer = Null | Ptr of string * node
+  and node = hash_pointer * Data.t
+
+  let hash_of_node = function
+    | (Ptr (h, _), d) -> h ^ Data.hash d
+    | (Null, d) -> Data.hash d
+
+  let rec verify = function
+    | Null -> true
+    | Ptr (h, ((ptr, _) as n)) -> if h = hash_of_node n then verify ptr else false
+
+  let empty = Null
+
+  let add h d = let n = (h, d) in Ptr (hash_of_node n, n) 
+
+  let rec add_list h = function
+    | d :: ds -> add_list (add h d) ds
+    | [] -> h
+
+  (** Printing functionality *)
+  let string_of_ptr = function
+    | Null -> "Null"
+    | Ptr (h, _) -> "Hash: [" ^ h ^ "]"
+
+  let string_of_data d = "Data: [" ^ Data.string_of_t d ^ "]"
+
+  let string_of_node num ptr d = "Node " ^ string_of_int num ^ ": [" ^ 
+    string_of_ptr ptr ^ ", " ^ string_of_data d ^ "]"
+
+  let print_chain ptr = 
+    let rec print_ptr num = function
+      | Null -> ()
+      | Ptr (_, n) -> print_node num n
+    and print_node num (ptr, d) = 
+      print_endline (string_of_node num ptr d) ; print_ptr (num + 1) ptr
+    in string_of_ptr ptr |> print_endline ; print_ptr 1 ptr
+
+  let rec print_chain_old = function
+    | Null -> print_endline "Null"
+    | Ptr (_, (next, d)) as ptr -> begin
+      print_endline ("Node: [" ^ string_of_ptr ptr ^ ", " ^ string_of_data d ^ "]") ; 
+      print_chain_old next
+    end
+end
+
+
 module String_blockchain = Make_blockchain(struct
     type t = string
     let hash s = s
+    let string_of_t s = s
 end)
 
 let () =  
   let s = String_blockchain.empty in
   let s_list = "s1" :: "s2" :: "s3" :: "s4" :: "s5" :: [] in
   let s1 = String_blockchain.add_list s s_list in
-  String_blockchain.verify s1 |> Printf.printf "%B"
+  let () = String_blockchain.verify s1 |> Printf.printf "%B\n" in
+  let () = String_blockchain.print_chain_old s1 in
+  String_blockchain.print_chain s1
